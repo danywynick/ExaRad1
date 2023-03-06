18510952feab4619ba0321cf5afb4de573b907ae
usuario <usuario@LAPTOP-N08BQ166>
2023-03-06 16:38:09 -0600

    Agregar archivos de proyecto.

---
+++ ExaRad1/ExaRad1/Pagecontacto.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ExaRad1
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Pagecontacto : ContentPage
+    {
+        public Pagecontacto()
+        {
+            InitializeComponent();
+        }
+        private async void Btmostrar_clicked(object sender, EventArgs e)
+        {
+            await DisplayAlert("Llene todos los campos");
+        }
+
+        private Task DisplayAlert(string v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    }
+
+   
---
+++ ExaRad1/ExaRad1/modelcontacto/Contac1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExaRad1.modelcontacto
+{
+    public class Contac1
+    {
+        int  id { get; set; }
+        public string nombre { get; set; }
+         public string apellidos { get; set; }
+         public string edad { get; set; }
+
+        public string pais { get; set; }
+    }
+}
